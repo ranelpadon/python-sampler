3edc33a9e91b10bf5a3f41843fc3c47cec7adfe7
ranelpadon <ranel.padon@gmail.com>
2014-05-06 01:46:21 +0800

    Improved the code documentation.

--- map_generator_countries/generate_countries.py
+++ map_generator_countries/generate_countries.py
@@ -1,5 +1,8 @@
 import os, urllib, json
 
+# Specify the filename to used as input.
+FILENAME = "COUNTRIES.txt"
+
 # Specify the map parameters to be used.
 ZOOM_LEVEL_MIN = 3
 ZOOM_LEVEL_MAX = 8
@@ -7,15 +10,21 @@ IMAGE_WIDTH = "600"
 IMAGE_HEIGHT = "600"
 
 # Check if the target storage folder exists, create if not yet existing.
-# It will overwrite the previously fetched map images.
 if not os.path.isdir("generated_maps"):
   os.mkdir("generated_maps")
 
-with open('COUNTRIES.txt') as countries:
+# Open the list of Countries to be parsed.
+# The 'with' keyword has auto-close mechanism when
+#  it has finisihed reading the input file.
+# Hence, we do not need to add exception handler.
+# Note that it will overwrite the previously fetched map images.
+with open(FILENAME) as countries:
   for country in countries:
+    # Remove all leading and trailing spaces per line.
     country = country.strip()
 
-    # Other countries could not be geocoded properly.
+    # Some countries could not be geocoded properly.
+    # They need some equivalent names.
     if country == "Congo, the Democratic Republic of the":
       country = "DR Congo"
 
@@ -25,19 +34,24 @@ with open('COUNTRIES.txt') as countries:
     elif country == "Virgin Islands, British":
       country = "British Virgin Islands"
 
+    # Since I/O operations are major bottlenecks, comment this line
+    # if you need to accelerate the map fetching.
     print "Processing: " + country + " Country"
 
     # This will be used in the filenames of the generated images.
     country_raw = country
 
-    # Encode special charcters.
+    # URL Encode special charcters before we pass it to the Geocoder.
     # This is necessary especially for Macintosh systems.
     country_encoded = urllib.quote_plus(country)
 
-    # Utilize the Google's Geocoding API
+    # Utilize the Google's Geocoding API.
     url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + country_encoded + "&sensor=false"
 
+    # Send a request for the Geocoded object.
     response = urllib.urlopen(url);
+
+    # Read the response and pipe it to the JSON loader.
     data = json.loads(response.read())
 
     # Retrieve the Latitude and Longitude values from the Geocoded JSON object.
@@ -46,6 +60,8 @@ with open('COUNTRIES.txt') as countries:
 
     # Fetch all zoom levels for the current country.
     for zoom in range(ZOOM_LEVEL_MIN, ZOOM_LEVEL_MAX + 1):
+      # Since I/O operations are major bottlenecks, comment this line
+      # if you need to accelerate the map fetching.
       print "\tGenerating Zoom Level: " + str(zoom)
 
       # Specify the filenmae structure/format.
