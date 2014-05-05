79dfaa295ad71a20911b117ee41331e8bce54f23
ranelpadon <ranel.padon@gmail.com>
2014-05-06 01:34:45 +0800

    Improved the codebase. Added handler for creating folder for the fetched map images. Improved the file naming of the saved map images. Added code documentation.

--- map_generator_countries/generate_countries.py
+++ map_generator_countries/generate_countries.py
@@ -1,10 +1,15 @@
-import urllib, json
+import os, urllib, json
 
+# Specify the map parameters to be used.
 ZOOM_LEVEL_MIN = 3
 ZOOM_LEVEL_MAX = 8
 IMAGE_WIDTH = "600"
 IMAGE_HEIGHT = "600"
-count = 0
+
+# Check if the target storage folder exists, create if not yet existing.
+# It will overwrite the previously fetched map images.
+if not os.path.isdir("generated_maps"):
+  os.mkdir("generated_maps")
 
 with open('COUNTRIES.txt') as countries:
   for country in countries:
@@ -22,35 +27,31 @@ with open('COUNTRIES.txt') as countries:
 
     print "Processing: " + country + " Country"
 
-    country = urllib.quote_plus(country)
+    # This will be used in the filenames of the generated images.
+    country_raw = country
 
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + country + "&sensor=false"
+    # Encode special charcters.
+    # This is necessary especially for Macintosh systems.
+    country_encoded = urllib.quote_plus(country)
+
+    # Utilize the Google's Geocoding API
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + country_encoded + "&sensor=false"
 
     response = urllib.urlopen(url);
     data = json.loads(response.read())
 
+    # Retrieve the Latitude and Longitude values from the Geocoded JSON object.
     LATITUDE = data["results"][0]["geometry"]["location"]["lat"]
     LONGITUDE = data["results"][0]["geometry"]["location"]["lng"]
 
-    for zoom in range(ZOOM_LEVEL_MIN, ZOOM_LEVEL_MAX+1):
+    # Fetch all zoom levels for the current country.
+    for zoom in range(ZOOM_LEVEL_MIN, ZOOM_LEVEL_MAX + 1):
       print "\tGenerating Zoom Level: " + str(zoom)
 
-      filename = country + " - " + str(zoom) + ".png"
+      # Specify the filenmae structure/format.
+      filename = "generated_maps/" + country_raw + " - " + str(zoom) + ".png"
 
+      # Utilize the static images of Nokia Maps.
       urllib.urlretrieve("http://image.maps.cit.api.here.com/mia/1.6/?app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg&w=" + IMAGE_WIDTH + "&h="+ IMAGE_HEIGHT + "&t=8&z=" + str(zoom) + "&lat=" + str(LATITUDE) + "&lon=" + str(LONGITUDE), filename)
 
     print
-
-
-
-
-
-
-
-
-
-
-
-
-
-
